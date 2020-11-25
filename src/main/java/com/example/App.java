ef9ab7297f3ddf2ffe5197b7ad9a160fc14b1330
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-11-25 14:06:03 +0800

    start code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,14 +2,10 @@ package com.example;
 
 import org.javamoney.moneta.Money;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class App
 {
     public static void main( String[] args )
     {
-        List<String> textList = new ArrayList();
         Money money = Money.of(100, "USD");
         System.out.println( "Hello World! I have money: " + money);
     }
